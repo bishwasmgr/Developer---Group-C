1ab9c80228addafea70b408ac8dd70f9b7fd93e9
nithyayamsinghe <nitheeromeshi@gmail.com>
2024-08-06 23:09:00 +0930

    Nithya S370170 Week01 and Week02

---
+++ S370170/Week - 02/python-example.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+"""Top-level script to invoke helloworld implementation."""
+
+import sys
+import helloworld.main
+
+if __name__ == '__main__':
+    sys.exit(helloworld.main.main())
